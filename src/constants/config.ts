734c5f7e4d2851c8a4824f5da6096b22e74bca37
LostZookeeper <102682249+LostZookeeper@users.noreply.github.com>
2022-03-30 20:14:16 +0200

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
   language: 'Rumantsch', // This changes the display name for your language
-  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Angelica', // Put your name here so people know who made this Wordle!
   authorWebsite: 'https://www.drg.ch/startseite', // Put a link to your website or social media here
   wordListSource: 'Dicziunari Rumantsch Grischun', // Describe the source material for your words here
