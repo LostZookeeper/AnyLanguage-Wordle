2cd3f391ce8d36120523cc205e9567c058b1a346
LostZookeeper <102682249+LostZookeeper@users.noreply.github.com>
2022-03-30 19:01:43 +0200

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: 'Rumantsch', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  author: 'Angelica', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'https://www.drg.ch/startseite', // Put a link to your website or social media here
+  wordListSource: 'Dicziunari Rumantsch Grischun', // Describe the source material for your words here
+  wordListSourceLink: 'http://online.drg.ch', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
