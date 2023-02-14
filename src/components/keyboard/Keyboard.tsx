78f0321559664665e0ca2d11464b0443e2067649
LostZookeeper <102682249+LostZookeeper@users.noreply.github.com>
2023-02-14 11:03:58 +0100

    Update Keyboard.tsx

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,100 +1,30 @@
-import { KeyValue } from '../../lib/keyboard'
-import { getStatuses } from '../../lib/statuses'
-import { Key } from './Key'
-import { useEffect } from 'react'
-import { ORTHOGRAPHY } from '../../constants/orthography'
-import { useTranslation } from 'react-i18next'
+import { KeyValue } from "../../lib/keyboard";
+import { getStatuses } from "../../lib/statuses";
+import { Key } from "./Key";
 
 type Props = {
-  onChar: (value: string) => void
-  onDelete: () => void
-  onEnter: () => void
-  guesses: string[][]
-}
+  onChar: (value: string) => void;
+  onDelete: () => void;
+  onEnter: () => void;
+  guesses: string[];
+};
 
 export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
-  const { t } = useTranslation()
-  const charStatuses = getStatuses(guesses)
+  const charStatuses = getStatuses(guesses);
+  console.log(charStatuses);
 
   const onClick = (value: KeyValue) => {
-    if (value === 'ENTER') {
-      onEnter()
-    } else if (value === 'DELETE') {
-      onDelete()
-    } else {
-      onChar(value)
+    if (value === "ENTER") {
+      return onEnter();
     }
-  }
-
-  useEffect(() => {
-    const listener = (e: KeyboardEvent) => {
-      if (e.code === 'Enter') {
-        onEnter()
-      } else if (e.code === 'Backspace') {
-        onDelete()
-      }
-      // Take away key event listener for now
-      // else {
-      //   const key = e.key.toUpperCase()
-      //   if (key.length === 1 && key >= 'A' && key <= 'Z') {
-      //     onChar(key)
-      //   }
-      // }
+    if (value === "DELETE") {
+      return onDelete();
     }
-    window.addEventListener('keyup', listener)
-    return () => {
-      window.removeEventListener('keyup', listener)
-    }
-  }, [onEnter, onDelete, onChar])
+    return onChar(value);
+  };
 
   return (
-//    <div>
-//      <div className="flex justify-center mb-1">
-//        {ORTHOGRAPHY.slice(0, Math.floor(ORTHOGRAPHY.length * 0.4)).map(
-//          (char) => (
-//            <Key
-//              key={char}
-//              value={char}
-//              onClick={onClick}
-//              status={charStatuses[char]}
-//            />
-//          )
-//        )}
-//      </div>
-//      <div className="flex justify-center mb-1">
-//        {ORTHOGRAPHY.slice(
-//          Math.floor(ORTHOGRAPHY.length * 0.4),
-//          Math.floor(ORTHOGRAPHY.length * 0.7)
-//        ).map((char) => (
-//          <Key
-//            key={char}
-//            value={char}
-//            onClick={onClick}
-//            status={charStatuses[char]}
-//          />
-//        ))}
-//      </div>
-//      <div className="flex justify-center">
-//        <Key key="enterKey" width={65.4} value="ENTER" onClick={onClick}>
-//          {t('enterKey')}
-//        </Key>
-//        {ORTHOGRAPHY.slice(
-//          Math.floor(ORTHOGRAPHY.length * 0.7),
-//          ORTHOGRAPHY.length
-//        ).map((char) => (
-//          <Key
-//            key={char}
-//            value={char}
-//            onClick={onClick}
-//            status={charStatuses[char]}
-//          />
-//        ))}
-//        <Key key="deleteKey" width={65.4} value="DELETE" onClick={onClick}>
-//          {t('deleteKey')}
-//        </Key>
-//      </div>
-//    </div>
-   <div>
+    <div>
       <div className="flex justify-center mb-1">
         <Key value="Q" onClick={onClick} status={charStatuses["Q"]} />
         <Key value="W" onClick={onClick} status={charStatuses["W"]} />
@@ -134,5 +64,5 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
         </Key>
       </div>
     </div>
-  )
-}
+  );
+};
