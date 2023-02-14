ae670c6bedeff9344ae71e1befb0d37ebdcfeeab
LostZookeeper <102682249+LostZookeeper@users.noreply.github.com>
2023-02-14 11:01:05 +0100

    Update Keyboard.tsx

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -48,49 +48,89 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
   }, [onEnter, onDelete, onChar])
 
   return (
-    <div>
+//    <div>
+//      <div className="flex justify-center mb-1">
+//        {ORTHOGRAPHY.slice(0, Math.floor(ORTHOGRAPHY.length * 0.4)).map(
+//          (char) => (
+//            <Key
+//              key={char}
+//              value={char}
+//              onClick={onClick}
+//              status={charStatuses[char]}
+//            />
+//          )
+//        )}
+//      </div>
+//      <div className="flex justify-center mb-1">
+//        {ORTHOGRAPHY.slice(
+//          Math.floor(ORTHOGRAPHY.length * 0.4),
+//          Math.floor(ORTHOGRAPHY.length * 0.7)
+//        ).map((char) => (
+//          <Key
+//            key={char}
+//            value={char}
+//            onClick={onClick}
+//            status={charStatuses[char]}
+//          />
+//        ))}
+//      </div>
+//      <div className="flex justify-center">
+//        <Key key="enterKey" width={65.4} value="ENTER" onClick={onClick}>
+//          {t('enterKey')}
+//        </Key>
+//        {ORTHOGRAPHY.slice(
+//          Math.floor(ORTHOGRAPHY.length * 0.7),
+//          ORTHOGRAPHY.length
+//        ).map((char) => (
+//          <Key
+//            key={char}
+//            value={char}
+//            onClick={onClick}
+//            status={charStatuses[char]}
+//          />
+//        ))}
+//        <Key key="deleteKey" width={65.4} value="DELETE" onClick={onClick}>
+//          {t('deleteKey')}
+//        </Key>
+//      </div>
+//    </div>
+   <div>
       <div className="flex justify-center mb-1">
-        {ORTHOGRAPHY.slice(0, Math.floor(ORTHOGRAPHY.length * 0.4)).map(
-          (char) => (
-            <Key
-              key={char}
-              value={char}
-              onClick={onClick}
-              status={charStatuses[char]}
-            />
-          )
-        )}
+        <Key value="Q" onClick={onClick} status={charStatuses["Q"]} />
+        <Key value="W" onClick={onClick} status={charStatuses["W"]} />
+        <Key value="E" onClick={onClick} status={charStatuses["E"]} />
+        <Key value="R" onClick={onClick} status={charStatuses["R"]} />
+        <Key value="T" onClick={onClick} status={charStatuses["T"]} />
+        <Key value="Y" onClick={onClick} status={charStatuses["Y"]} />
+        <Key value="U" onClick={onClick} status={charStatuses["U"]} />
+        <Key value="I" onClick={onClick} status={charStatuses["I"]} />
+        <Key value="O" onClick={onClick} status={charStatuses["O"]} />
+        <Key value="P" onClick={onClick} status={charStatuses["P"]} />
       </div>
       <div className="flex justify-center mb-1">
-        {ORTHOGRAPHY.slice(
-          Math.floor(ORTHOGRAPHY.length * 0.4),
-          Math.floor(ORTHOGRAPHY.length * 0.7)
-        ).map((char) => (
-          <Key
-            key={char}
-            value={char}
-            onClick={onClick}
-            status={charStatuses[char]}
-          />
-        ))}
+        <Key value="A" onClick={onClick} status={charStatuses["A"]} />
+        <Key value="S" onClick={onClick} status={charStatuses["S"]} />
+        <Key value="D" onClick={onClick} status={charStatuses["D"]} />
+        <Key value="F" onClick={onClick} status={charStatuses["F"]} />
+        <Key value="G" onClick={onClick} status={charStatuses["G"]} />
+        <Key value="H" onClick={onClick} status={charStatuses["H"]} />
+        <Key value="J" onClick={onClick} status={charStatuses["J"]} />
+        <Key value="K" onClick={onClick} status={charStatuses["K"]} />
+        <Key value="L" onClick={onClick} status={charStatuses["L"]} />
       </div>
       <div className="flex justify-center">
-        <Key key="enterKey" width={65.4} value="ENTER" onClick={onClick}>
-          {t('enterKey')}
+        <Key width={65.4} value="ENTER" onClick={onClick}>
+          Enter
         </Key>
-        {ORTHOGRAPHY.slice(
-          Math.floor(ORTHOGRAPHY.length * 0.7),
-          ORTHOGRAPHY.length
-        ).map((char) => (
-          <Key
-            key={char}
-            value={char}
-            onClick={onClick}
-            status={charStatuses[char]}
-          />
-        ))}
-        <Key key="deleteKey" width={65.4} value="DELETE" onClick={onClick}>
-          {t('deleteKey')}
+        <Key value="Z" onClick={onClick} status={charStatuses["Z"]} />
+        <Key value="X" onClick={onClick} status={charStatuses["X"]} />
+        <Key value="C" onClick={onClick} status={charStatuses["C"]} />
+        <Key value="V" onClick={onClick} status={charStatuses["V"]} />
+        <Key value="B" onClick={onClick} status={charStatuses["B"]} />
+        <Key value="N" onClick={onClick} status={charStatuses["N"]} />
+        <Key value="M" onClick={onClick} status={charStatuses["M"]} />
+        <Key width={65.4} value="DELETE" onClick={onClick}>
+          Delete
         </Key>
       </div>
     </div>
